4508344932130349fc35fbb506ac774d3c1bb8dd
oriioll <oriolplazas2@gmail.com>
2025-11-27 18:04:51 +0100

    style: Add css variables for diferent themes

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+
+//Get all icons id
+const fcbButton = document.getElementById('fcb')
+const kobeButton = document.getElementById('kobe')
+const total90Button = document.getElementById('total90')
+
